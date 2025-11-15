b0fbb201d0f869ca70f39affb2bf6e0c06154a0c
jknate <48931286+jknate@users.noreply.github.com>
2025-11-14 21:33:34 -0500

    add lab5

--- app/Labs/page.tsx
+++ app/Labs/page.tsx
@@ -23,6 +23,16 @@ export default function Labs() {
             Lab 3: JavaScript Fundamentals
           </Link>
         </li>
+        <li>
+          <Link href="/Labs/Lab4" id="wd-lab4-link">
+            Lab 4: React State and Props
+          </Link>
+        </li>
+        <li>
+          <Link href="/Labs/Lab5" id="wd-lab5-link">
+            Lab 5: Node.js and HTTP Client
+          </Link>
+        </li>
       </ul>
     </div>
   );
